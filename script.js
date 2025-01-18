7cd99a142a314087a3e3b47e360df7da5bc00a5f
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-18 23:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+ let obj = {}
+ for(let i=0; i<str.length; i++) {
+   if(obj[str[i]]){
+     obj[str[i]]++;
+   }else{
+     obj[str[i]] = 1;
+   }
+ }
+ 
+ let keys = Object.keys(obj);
+ for(let key of keys) {
+   if(obj[key] === 1) {
+     return key
+   }
+ } 
+ 
+ return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
